0a2031e5c4379285bab324a4a5352bd1f1a5e8af
Lukas Stein <besonderskreativ@gmail.com>
2022-09-18 00:05:50 +0200

    Added files[B

--- aliveled.py
+++ aliveled.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
  
-p = GPIO.PWM(23,500)  # frequency=50Hz
+p = GPIO.PWM(23, 500)  # frequency=500Hz
 p.start(0)
 try:
     while 1:
         for dc in range(0, 51, 5):
             p.ChangeDutyCycle(0)
-            time.sleep(0.5) 
+            time.sleep(0.5)
             p.ChangeDutyCycle(dc)
             time.sleep(0.1)
         for dc in range(50, -1, -5):
