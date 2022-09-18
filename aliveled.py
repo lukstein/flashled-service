4c339a201cec25420f47934588c65a2cfcd4139c
Lukas Stein <besonderskreativ@gmail.com>
2022-09-18 20:34:02 +0200

    Changed frequency to comply better with Kemo M240

--- aliveled.py
+++ aliveled.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
  
-p = GPIO.PWM(23, 500)  # frequency=500Hz
+p = GPIO.PWM(23, 200)  # frequency=200Hz
 p.start(0)
 try:
     while 1:
